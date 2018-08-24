ab541c48de0a6771f5660d78df5eeb7442231de9
Jayvz Olazo <jayvzolazo@gmail.com>
2018-08-24 13:41:35 +0800

    side selection behavior

--- src/app/persons-sidebar/persons-sidebar.component.ts
+++ src/app/persons-sidebar/persons-sidebar.component.ts
@@ -13,12 +13,17 @@ export class PersonsSidebarComponent implements OnInit {
   @Output() setPerson = new EventEmitter<any>();
 
   persons = PERSONS;
+  selected = [];
 
   constructor() { }
 
   ngOnInit() { }
 
-  onSelect(person: Person): void {
+  onSelect(person: Person, index: number): void {
+    // clear any selection and set the selected index to active mode
+    this.selected = [];
+    this.selected[index] = true;
+
     // event usage
     this.setPerson.emit(person);
   }
