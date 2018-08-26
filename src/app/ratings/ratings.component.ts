3841a535b2901ba1262569ac2e55574c8b48b6f6
Jayvz Olazo <jayvzolazo@gmail.com>
2018-08-26 17:38:38 +0800

    added person rate

--- src/app/ratings/ratings.component.ts
+++ src/app/ratings/ratings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ratings',
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ratings.component.scss']
 })
 export class RatingsComponent implements OnInit {
+  @Input() ratings: any;
+
+  rateMaxCount = 5;
+  profileRates = [
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+  rateCount = 0;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    this.profileRates = [
+      false,
+      false,
+      false,
+      false,
+      false
+    ];
+    this.rateCount = 0;
+
+    while (this.rateCount < this.ratings) {
+      this.profileRates[this.rateCount] = true;
+
+      this.rateCount++;
+    }
+
+    console.log('this.profileRates', this.profileRates);
+  }
 }
